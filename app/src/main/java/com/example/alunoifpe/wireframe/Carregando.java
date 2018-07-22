fe07b3a3dccd3d1a51730ed813a2faeb5c4fdcfe
TarcisioMG <tarcisiomarques00000@gmail.com>
2018-07-22 11:52:06 -0300

    atualizacao com inicio intent

--- app/src/main/java/com/example/alunoifpe/wireframe/Carregando.java
+++ app/src/main/java/com/example/alunoifpe/wireframe/Carregando.java
@@ -1,13 +1,16 @@
 package com.example.alunoifpe.wireframe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main5Activity extends AppCompatActivity {
+public class Carregando extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
+        setContentView(R.layout.carregando);
     }
+
+    Intent intent = getIntent();
 }
